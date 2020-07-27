42b317d5d3c1b4b4505fe13eb9dd99e538e940f5
takutaka <taku.takahashi120@gmail.com>
2020-07-27 22:52:48 +0900

    BTC_JPY

--- lib/parser.py
+++ lib/parser.py
@@ -2,7 +2,7 @@ import argparse
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ticker", required=True, choices=["ETH_JPY"])
+    parser.add_argument("--ticker", required=True, choices=["ETH_JPY", "BTC_JPY"])
     parser.add_argument("--jpy", required=True)
     args = parser.parse_args()
     return args
