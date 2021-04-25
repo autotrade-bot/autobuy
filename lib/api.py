8a7ef8d96ef1a04cc1921372cab4fa7dd14d6314
takutaka <taku.takahashi120@gmail.com>
2021-04-25 18:13:13 +0900

    fix env name

--- lib/api.py
+++ lib/api.py
@@ -1,5 +1,5 @@
 import pybitflyer
 import os
 def initialize():
-    api = pybitflyer.API(api_key=os.environ["API_KEY"], api_secret=os.environ["API_SECRET"])
+    api = pybitflyer.API(api_key=os.environ["BITFLYER_API_KEY"], api_secret=os.environ["BITFLYER_API_SECRET"])
     return api
